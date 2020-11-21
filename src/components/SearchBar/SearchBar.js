2e177b9fc8fb642e35972395d9b330358eb06ce9
kproudler <kevinproudler@gmail.com>
2020-11-21 18:50:48 -0500

    enabled search with enter key

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -19,6 +19,8 @@ class SearchBar extends React.Component {
 
         this.handleSearch = this.handleSearch.bind(this);
 
+        this.handleKeyPress = this.handleKeyPress.bind(this);
+
         this.sortByOptions = {
             'Best Match': 'best_match',
             'Highest Rated': 'rating',
@@ -56,8 +58,14 @@ class SearchBar extends React.Component {
 
     handleSearch(e){
         this.props.searchYelp(this.state.term, this.state.location, this.state.sortBy)
-        e.preventDefault();
+        // e.preventDefault();
     };
+
+    handleKeyPress(e) {
+        if(e.which === 13) {
+            this.handleSearch();
+        }
+    }
 
     render() {
         return(
@@ -68,8 +76,8 @@ class SearchBar extends React.Component {
                 </ul>
             </div>
             <div className="SearchBar-fields">
-                <input placeholder="Search Businesses" onChange={this.handleTermChange}/>
-                <input placeholder="Where?" onChange={this.handleLocationChange}/>
+                <input placeholder="Search Businesses" onChange={this.handleTermChange} onKeyPress={this.handleKeyPress}/>
+                <input placeholder="Where?" onChange={this.handleLocationChange} onKeyPress={this.handleKeyPress}/>
             </div>
             <div className="SearchBar-submit">
                 <a onClick={this.handleSearch}>Let's Go</a>
